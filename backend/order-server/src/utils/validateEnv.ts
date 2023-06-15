7514d62c2db09dfea0a293583b7945da87205d6e
IainMosima <iainmosima@gmail.com>
2023-06-15 17:35:04 +0300

    refactored orders server

---
+++ backend/order-server/src/utils/validateEnv.ts
@@ -0,0 +1,15 @@
+import { cleanEnv } from "envalid";
+import { port, str, num } from "envalid/dist/validators";
+
+// exporting validated environment variables
+export default cleanEnv(process.env, {
+    MONGO_CONNECTION_STRING: str(),
+    PORT: port(),
+    SESSION_SECRETY_KEY: str(),
+    MPESACONSUMERKEY: str(),
+    MPESACONSUMERSECRETKEY: str(),
+    MPESAPASSKEY: str(),
+    MPESABUSINESSSHORTCODE: num(),
+    MPESAACCOUNTREFERENCE: str(),
+    ENVIRONMENT: str(),
+});
