3803dc9d977cba2b4a73f272c5853b5d18f12d0c
IainMosima <iainmosima@gmail.com>
2023-09-11 05:00:03 +0300

    updated yaml files

--- users-server/src/routes/user.ts
+++ users-server/src/routes/user.ts
@@ -12,10 +12,21 @@ router.get('/', requireAuth, UsersController.getAuthenticatedUser);
 // creating a new user
 router.post('/signup', upload.single('profileImg'), UsersController.signup);
 
+// updating a user's profile
+router.patch('/update/:userId', upload.single('profileImg'), UsersController.updateUserProfile);
+
 // logging out a user
 router.post('/logout', UsersController.logout);
 
 // logging in a user
 router.post('/login', UsersController.login);
+
+// checking if username exists
+router.get('/checkusername/:query', UsersController.checkUername);
+
+// checking if email exists
+router.get('/checkemail/:query', UsersController.checkEmail);
+
+
 
 export default router;
