8b35eab0ef09f7fc496baeedc0816883dfe49965
Iain Mosima <iain.nyakundi@apollo.ac.ke>
2024-09-10 09:35:19 +0300

    final changes

--- products-server/src/models/category.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var categorySchema = new mongoose_1.Schema({
-    categories: { type: Array }
-});
-exports.default = (0, mongoose_1.model)('Category', categorySchema);
